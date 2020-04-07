18ba84988217c27e042269be65ca054d76274549
Donaim <3Donaim@gmail.com>
2020-04-07 19:23:06 +0200

    refactor server code

--- server/index.js
+++ server/index.js
@@ -2,39 +2,44 @@
 // process.chdir('src')
 __dirname = process.cwd();
 
-function start_router() {
-    var fs = require("fs");
-    var http = require("http");
-    var url = require("url");
-    var path = require("path");
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const path = require("path");
 
+function serve_static(filename, callback) {
+    function callback0(err, data) {
+        if (err) {
+            const fullname = path.join(__dirname, filename);
+            fs.readFile(fullname, "utf8", callback)
+        } else {
+            callback(err, data);
+        }
+    }
+    fs.readFile(filename, "utf8", callback0);
+}
+
+function start_router() {
     var server = http.createServer(function (request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
         response.writeHead(200, {"Content-Type": "text/html"});
 
-        var fullname = path.join(__dirname, pathname);
-        // console.log("!", fullname, "!");
-        // console.log("exists?", fs.existsSync(fullname));
-
-        if(pathname == "/") {
-            var html = fs.readFileSync("index.html", "utf8");
-            response.write(html);
-        }
-        else if (fs.existsSync(fullname)) {
-            var script = fs.readFileSync(fullname, "utf8");
-            response.write(script.toString());
-        }
-        else if (fs.existsSync(pathname)) {
-            var script = fs.readFileSync(pathname, "utf8");
-            response.write(script.toString());
-        }
-        else {
-            console.error("Unknown route request = \"" + pathname + "\"");
+        function static_callback(err, data) {
+            if (err) {
+                // response.writeHead(404, {"Content-Type": "text/html"});
+                // response.write("<p>Page not found</p>");
+                console.error("Unknown route request = \"" + pathname + "\" (" + err + ")");
+            } else {
+                // response.writeHead(200, {"Content-Type": "text/html"});
+                response.write(data);
+            }
+            response.end();
         }
 
-        response.end();
+        const realPath = pathname === "/" ? "index.html" : pathname;
+        return serve_static(realPath, static_callback);
     });
 
     var port = process.env.PORT || 1337;
