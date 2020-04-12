fe35056c4011c1644781298e04eaabcd850bbbe3
Donaim <3Donaim@gmail.com>
2020-04-12 15:27:07 +0200

    auto-scroll

--- react-app/src/HistoryView.js
+++ react-app/src/HistoryView.js
@@ -29,12 +29,26 @@ class HistoryView extends Component {
         setInterface('history:add-item', addItem, args.ictx);
     }
 
+    scrollToBottom = () => {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
     render() {
         const hist = this.state.hist;
         const indexes = range(hist.length).map(x => 'historyElement#' + x)
         const ziped = zip(hist, indexes);
         const maped = ziped.map(x => <HistoryElement elem={x[0]} key={x[1]} />)
-        return (<div style={this.style}> <div style={this.dstyle} > {maped} </div> </div>);
+        return (<div style={this.style}>
+                <div style={this.dstyle} >
+                    {maped}
+                <div style={{ float:"left", clear: "both" }}
+                     ref={(el) => { this.messagesEnd = el; }}>
+                </div>
+                </div>
+                </div>);
     }
 }
 
