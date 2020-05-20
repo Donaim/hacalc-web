7a88a71fcd0611a05e68b9c142d87456c5e3912a
Donaim <3Donaim@gmail.com>
2020-05-20 19:26:14 +0200

    history element: support hiding

--- react-app/src/HistoryElement.js
+++ react-app/src/HistoryElement.js
@@ -8,18 +8,35 @@ class HistoryElement extends Component {
         const elem = args.elem;
         const isString = typeof elem == 'string';
         const isResponse = isString ? false : elem.isResponse;
+        this.isInternal = elem.isInternal;
         this.text = isString ? elem : elem.text;
-        this.styles = isResponse ? { backgroundColor: '#61dafb', border: '1px solid #61dafb' } : { backgroundColor: 'white' } ;
-        this.styles.width = '100%';
-        this.styles.display = elem.isInternal? 'none' : 'auto';
+
+        const baseStyles = isResponse ? { backgroundColor: '#61dafb', border: '1px solid #61dafb' } : { backgroundColor: 'white' };
+        baseStyles.width = '100%';
+        this.stylesVisible = { ... baseStyles, display: 'auto'};
+        this.stylesNotVisible = { ... baseStyles, display: 'none'};
+
+        this.state = {
+            hide: true,
+        };
+
+        this.setVisibility = (shouldHide) => {
+            if (this.isInternal) {
+                this.setState({ hide: shouldHide });
+            }
+        };
     }
 
     render() {
+        const st = (this.isInternal && this.state.hide)
+            ? this.stylesNotVisible
+            : this.stylesVisible;
+
         return (<div>
                 <input tabIndex={-1}
                        readOnly={true}
                        value={this.text}
-                       style={this.styles}
+                       style={st}
                        />
                 </div>);
     }
