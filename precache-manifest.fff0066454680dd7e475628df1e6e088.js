5c03187ddfc5a535009d5611603d8e9552177e2b
Donaim <Donaim@donaim.donaim>
2021-06-12 20:57:43 +0200

    Updates

---
+++ precache-manifest.fff0066454680dd7e475628df1e6e088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629b8afdf8a2dc930468d2a8a1e4cab3",
+    "url": "/hacalc-web/index.html"
+  },
+  {
+    "revision": "377b5511c6b11ea95463",
+    "url": "/hacalc-web/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "cb3f9dbf3b81b3cb2825",
+    "url": "/hacalc-web/static/css/main.4e0d0ce6.chunk.css"
+  },
+  {
+    "revision": "377b5511c6b11ea95463",
+    "url": "/hacalc-web/static/js/2.55ae50fc.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/hacalc-web/static/js/2.55ae50fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3f9dbf3b81b3cb2825",
+    "url": "/hacalc-web/static/js/main.92bb9a48.chunk.js"
+  },
+  {
+    "revision": "6ec607914f93c55ae360",
+    "url": "/hacalc-web/static/js/runtime-main.3a8da2d7.js"
+  }
+]);
